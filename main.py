87469a9e3d23a0715156a4c80e5628ba035e91a2
Spotspotspot <b7u8xsv63g@liamekaens.com>
2022-03-12 22:55:30 +1000

    rearranged to remove the main function

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
 from graphics import *
 
-displayArea = GraphWin('Object Invaders', 300, 300)     # create graphical window object
-
-def main():
-
-    displayArea.setBackground("black")
-
-    # make some aliens. Fred, tom and harry are "Objects"
-    fred = Alien()          # make fred
-    fred.x = 50             # change fred's properties
-
-    tom = Alien()
-    tom.x = 200
-    tom.colour = "red"
-
-    harry = Alien()
-    harry.x = 100
-    harry.colour = "blue"
-
-    # do something with them
-    while (1):
-        tom.draw()
-        fred.draw()
-        harry.draw()
-
-        time.sleep(0.05)
-
-        tom.erase()
-        fred.erase()
-        harry.erase()
-
-        tom.move()
-        fred.move()
-        harry.move()
-
-
-class Alien: # this is a "Class" (obviously)
+# definition of Alien "Class"
+class Alien:
 
     # these are "Properties"
-    colour = "green"   # these are all default settings
+    colour = "green"   # these are the default properties
     x = 10
     y = 10
     direction = 1       # 1 = right, -1 = left
@@ -49,9 +15,9 @@ class Alien: # this is a "Class" (obviously)
         cir = Circle(Point(self.x, self.y), 20)     # create circle object
         cir.setOutline(self.colour)                 # set its colour property to what we want
         cir.setWidth(4)                             # set its line width to what we want
-        cir.draw(displayArea)                       # tell it to draw itself on the display object
+        cir.draw(displayArea)                       # tell it to draw itself on the graphical window object
 
-    def erase(self):
+    def erase(self):                                # same as draw(self): above except draws in black
         cir = Circle(Point(self.x, self.y), 20)
         cir.setOutline("black")
         cir.setWidth(4)
@@ -73,12 +39,47 @@ class Alien: # this is a "Class" (obviously)
                 self.direction = 1              # and head in right direction
 
         if self.y > 300:                        # check if we have fallen off the bottom
-           self.y = 10                          # restart at top (they dont really do this)
+           self.y = 10                          # restart at top (aliens don't really do this)
+
+# end of Alien class definition
 
 
+# Start of the Main program
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main()
+displayArea = GraphWin('Object Invaders', 300, 300)     # create graphical window object
+
+displayArea.setBackground("black")
+
+# create some aliens.
+# fred, tom and harry are "Objects"
+fred = Alien()          # create fred
+fred.x = 50             # change fred's properties
+
+tom = Alien()           # create tom
+tom.x = 200             # change tom's properties
+tom.colour = "red"
+
+harry = Alien()
+harry.x = 100
+harry.colour = "blue"
+
+# do something with them
+while (1):
+    tom.draw()
+    fred.draw()
+    harry.draw()
+
+    time.sleep(0.05)
+
+    tom.erase()
+    fred.erase()
+    harry.erase()
+
+    tom.move()
+    fred.move()
+    harry.move()
+
+    # and do it all again forever!
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
